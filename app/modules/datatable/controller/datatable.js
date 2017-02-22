3f6d52c5b2e005c7a1a2849f4ff6927da7422e26
Bastien Gibrat <bastien.gibrat@gmail.com>
2017-02-22 20:15:22 +0100

    Move delete button, add modal for "info" on datatable

--- app/modules/datatable/controller/datatable.js
+++ app/modules/datatable/controller/datatable.js
@@ -7,7 +7,7 @@
  * @description Lead the datatable list
  */
 angular.module('datatable')
-  .controller('DatatableCtrl', function (DatatableFactory, ItemManager, DatatableResolve, CategoryResolve) {
+  .controller('DatatableCtrl', function ($filter, DatatableFactory, ItemManager, DatatableResolve, CategoryResolve) {
 
     function Datatable() {
       this.data = DatatableResolve;
@@ -25,5 +25,18 @@ angular.module('datatable')
 
       return object.name;
     }
+
+    Datatable.prototype.modal = function($id) {
+      var expense = _.find(this.data, function(obj) {
+        return obj._id === $id;
+      });
+
+      bootbox.alert({
+          title: expense.name,
+          message: "<p>"+expense.amount+"</p><p>"+$filter('date')(expense.date)+"</p><p>"+this.getCategory(expense.category)+"</p>",
+          size: 'large'
+      });
+    }
+
     return new Datatable();
   });
